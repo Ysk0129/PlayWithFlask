e48e486a3e32361b791c59b6117e253a23699e3d
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-06 01:43:53 +0900

    Add Morphological analysis

---
+++ sampleapp/sampleapp/models/wordprocessing.py
@@ -0,0 +1,16 @@
+import MeCab
+
+class WordProcessing:
+
+    def __init__(self):
+        self.tagger = MeCab.Tagger("-Ochasen")
+    
+    def separate_words(self, sentence):
+        words = []
+        for i in self.tagger.parse(sentence).split("\n"):
+            parsed = i.split("\t")
+            if len(parsed) == 6:
+                if parsed[3].startswith("名詞") | parsed[3].startswith("動詞") | parsed[3].startswith("形容詞"):
+                    words.append(pased[2])
+        
+        return words
