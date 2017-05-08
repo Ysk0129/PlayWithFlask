4a7b05dee7be9836daf4840f55f3ef53927e0b71
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-09 00:36:35 +0900

    Update not to return 2 letters of Hiragana

--- sampleapp/sampleapp/models/wordprocessing.py
+++ sampleapp/sampleapp/models/wordprocessing.py
@@ -13,11 +13,13 @@ class WordProcessing:
             parsed = i.split("\t")
             if len(parsed) == 6:
                 if parsed[3].startswith("名詞") | parsed[3].startswith("動詞") | parsed[3].startswith("形容詞"):
-                    if re.search(r'[a-xA-Z0-9_]' ,parsed[2]) == None:
+                    if re.search(r'[a-xA-Z0-9_]' ,parsed[2]) == None and re.search(r'[ぁ-ん]{2}' ,parsed[2]) == None:
                         words.append(parsed[2])                     
         return words
 
     def count_words(self, words):
+        if len(words) < 1:
+            return []
         vec = CountVectorizer()
         vec.fit(words)
         terms = vec.get_feature_names()
