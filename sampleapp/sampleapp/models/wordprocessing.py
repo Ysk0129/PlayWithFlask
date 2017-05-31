8c37af0505bf8008c0a964cdfa5ca4a4d20468d7
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-01 02:36:46 +0900

    Clean the order of variables

--- sampleapp/sampleapp/models/wordprocessing.py
+++ sampleapp/sampleapp/models/wordprocessing.py
@@ -14,8 +14,8 @@ class WordProcessing:
             if len(parsed) == 6:
                 if parsed[3].startswith("名詞") | parsed[3].startswith("動詞") | parsed[3].startswith("形容詞"):
                     
-                    invalid_pattern = re.compile(r"([a-xA-Z0-9_!-/:-@[-`{-~]+)|([ぁ-ん]{1,2})")
                     valid_pattern = re.compile(r"(.{2,})|([一-龥].*)")
+                    invalid_pattern = re.compile(r"([a-xA-Z0-9_!-/:-@[-`{-~]+)|([ぁ-ん]{1,2})")
                     if re.fullmatch(valid_pattern, parsed[2]) is not None and re.fullmatch(invalid_pattern, parsed[2]) is None:
                         words.append(parsed[2])
 
