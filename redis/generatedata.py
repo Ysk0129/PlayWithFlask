191b6eab835ac417161b956901116821b2229c66
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-11 01:55:26 +0900

    Add Dockerfile of redis and generation and input of Japanese evaluation dictionary data

---
+++ redis/generatedata.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+
+    wago = []
+    with open("wago.121808.pn") as f:
+        for l in f.readlines():
+            line = l.replace("\t", "").replace(" ", "").replace("）", "）,").split(",")
+            
+            value = "1" if line[0].startswith("ポジ") else "-1"
+            key = line[1].rstrip("\n")
+            if key != "":
+                wago.append("SET " + "\"" + key + "\"" + " " + "\"" + value + "\"" + "\r\n")
+
+    with open("evaluation_dictionary.txt", "w") as f:
+        for l in wago:
+            f.write(l)
