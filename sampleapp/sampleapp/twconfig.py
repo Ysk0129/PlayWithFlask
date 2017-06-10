ded27ba839cf89308539eb72aa8027470506c514
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-10 22:56:30 +0900

    Add evaluation dictionary data

--- sampleapp/sampleapp/twconfig.py
+++ sampleapp/sampleapp/twconfig.py
@@ -1,6 +1,6 @@
 twkeys = {
-    'ConsumerKey': 'YOUR_CONSUMER_KEY',
-    'ConsumerSecret': 'YOUR_CONSUMER_SECRET',
-    'AccessToken': 'ACCESS_TOKEN',
-    'AccessTokenSecret': 'ACCESS_TOKEN_SECRET'
+    'ConsumerKey': 'b0tOFXJFZXINFb2DbUtIqx6Tk',
+    'ConsumerSecret': 'FcdnVsxKt95n98Et3HpqUmEBUVE7WhiVtMO7dYqHeKNk04z3Ib',
+    'AccessToken': '859679555035856896-oU8Uy8b7v7vD55J8L77k4pCoIqSH3rv',
+    'AccessTokenSecret': 'OBhQlIrbhsRPsXHstu05ifbclnBKGa4ES7soVIsL2XDyk'
 }
